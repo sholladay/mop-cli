576d9ae9ab19e8acec692fda8a1afe26fddbd124
Seth Holladay <me@seth-holladay.com>
2017-07-13 05:03:25 -0400

    Simplify actions

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 'use strict';
 
+const findProjects = require('./lib/find-projects');
 const outdated = require('./lib/action/outdated');
 const pinned = require('./lib/action/pinned');
 
 module.exports = {
-    outdated,
-    pinned
+    async outdated(cwd) {
+        const projects = await findProjects.withPkg(cwd);
+        return outdated(projects);
+    },
+    async pinned(cwd) {
+        const projects = await findProjects.withPkg(cwd);
+        return pinned(projects);
+    }
 };
