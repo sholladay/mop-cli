42bde5b1cdccac1a56e8e3fa8dc345ea4f343284
Seth Holladay <me@seth-holladay.com>
2017-08-22 14:19:20 -0400

    Fix caret-deps triggering on non-semver deps
    Use the semver-deps rule if you need the old behavior.

---
+++ test.js
@@ -0,0 +1,50 @@
+import test from 'ava';
+import caretDeps from './lib/rule/caret-deps';
+
+const makePackage = (pkg) => {
+    return { pkg };
+};
+
+test('rule: caret-deps', (t) => {
+    const nonPackage = {};
+    t.falsy(caretDeps({}), 'must ignore non-packages');
+    t.falsy(caretDeps(makePackage({})), 'must ignore empty packages');
+    t.falsy(caretDeps(makePackage({ dependencies : {} })), 'must ignore packages with no dependencies');
+
+    const nonSemver = makePackage({
+        dependencies : {
+            'build-path' : 'git@github.com:sholladay/build-path.git',
+            'build-data' : 'sholladay/build-data',
+            'build-keys' : 'latest',
+            'build-dir'  : './directory',
+            scube        : 'file:../foo/bar',
+            delivr       : 'https://example.com/ball.tar.gz'
+        }
+    });
+    t.falsy(caretDeps(nonSemver), 'must ignore non-semver dependencies');
+
+    const caret = makePackage({
+        dependencies : {
+            'build-path' : '^1.0.0'
+        }
+    });
+    t.falsy(caretDeps(caret), 'must pass for caret dependencies');
+
+    const pinned = makePackage({
+        dependencies : {
+            'build-path' : '1.0.0'
+        }
+    });
+    t.deepEqual(caretDeps(pinned), {
+        path    : 'package.json',
+        message : 'Package has pinned dependencies',
+        data    : {
+            dependencies : {
+                'build-path' : {
+                    expected : '^1.0.0',
+                    wanted   : '1.0.0'
+                }
+            }
+        }
+    });
+});
